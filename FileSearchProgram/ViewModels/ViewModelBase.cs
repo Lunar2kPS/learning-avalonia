73008408ba1fdff569c79e8ca30320c2a3654562
Carlos DaLomba <carlos@2kpixelstudios.net>
2026-02-12 02:11:54 -0600

    Created C# .NET 9.0 Avalonia MVVM project

---
+++ FileSearchProgram/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FileSearchProgram.ViewModels {
+    public abstract class ViewModelBase : ObservableObject {
+
+    }
+}
---
+++ FileSearchProgram/Views/MainWindow.axaml.cs
@@ -0,0 +1,9 @@
+using Avalonia.Controls;
+
+namespace FileSearchProgram.Views {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
